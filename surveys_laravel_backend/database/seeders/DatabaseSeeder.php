f694fcf3b56f648fa7bcee07952ed004b64ef450
Ahmad Othman <aothman156@gmail.com>
2022-06-20 23:51:56 +0300

    add: db seeders

--- surveys_laravel_backend/database/seeders/DatabaseSeeder.php
+++ surveys_laravel_backend/database/seeders/DatabaseSeeder.php
@@ -13,6 +13,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('users')->insert([
+            'id' => 1,
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('admin123'),
+        ]);
+        DB::table('users')->insert([
+            'id' =>2,
+            'name' => 'admin2',
+            'email' => 'admin2@gmail.com',
+            'password' => Hash::make('admin321'),
+        ]);
     }
 }
